0c4389a126c2539c143eec66fb1f953969ca8863
pavithranayak07 <pavinayak17@gmail.com>
2023-11-03 10:35:31 +0530

    first commit

---
+++ src/components/resume/Resume.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+
+function Resume() {
+  return (
+    <div>
+        <h1 className="res">Resume</h1>
+        <form>
+        <h1 className="pavi">Pavithra Nayak</h1>
+          <div className="abt"> 
+          
+          <p >pavinayak17@gmail.com</p>
+          <p >8786473980</p>
+          </div>
+          
+          
+          <h3 clssName="perfect">About:</h3>
+          <p clssName="perfect">Web developer with the knowledge og managing multiple webbase and web applivation design projects,<br></br>
+          developing creative and functional designs, web pages layouts,colournschemes and infrastructure.</p>
+
+          <div className="harm-np">
+          <h4 >Skills:</h4>
+          <p >C<br></br>Java<br></br>Python<br></br>HTML<br></br>CSS<br></br>Java Script<br></br>Communication<br></br>Leadership</p>
+          </div>
+         
+          
+          <h3 clssName="perfect">Education:</h3>
+          <p clssName="perfect">St.Lawrence English Medium High School,Moodubelle.<br></br>(2009-2019)<br></br>
+          St.Lawrence PU College,Moodubelle.<br></br>(2019-2021)<br></br>
+          Shri Madhwa Vadhiraja Institute of Technology and Management,Bantakal<br></br>(2021-2025)</p>
+          <h3 clssName="perfect">Achievements:</h3>
+          <p clssName="perfect">Participated in IEEE EUREKA.(2022 &2023)<br></br>
+          Participated in intra mini-project compitetion.(In 2023-Chordzooka)<br></br>
+          Participated in Security and Ethical hacking woekshop by NICC India.
+          
+          
+          </p>
+        </form>
+    </div>
+    
+    
+
+  )
+}
+
+export default Resume
